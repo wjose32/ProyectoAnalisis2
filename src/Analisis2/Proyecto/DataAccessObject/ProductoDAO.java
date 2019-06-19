672f679b70f9cd44542506aee23fb6ed6b0b9401
pedro <pedrov1.pv@gmail.com>
2019-06-18 20:54:44 -0600

    Se agrego el paquete Data Access Object Se agrego el archivo de ArgoUML, que contiene el diagrama de clases y los diagramas de caso de uso.

---
+++ src/Analisis2/Proyecto/DataAccessObject/ProductoDAO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analisis2.Proyecto.DataAccessObject;
+import java.util.Vector;
+
+/**
+ *
+ * @author pedro
+ */
+public class ProductoDAO {
+    public Vector  CreaUsa;
+    public void registarProducto() {
+    }
+
+    public void obtenerProducto() {
+    }
+
+    public void modificarProducto() {
+    }
+
+    public void actualizarProducto() {
+    }
+
+    public void obtenerProductocategoria() {
+    }
+}
